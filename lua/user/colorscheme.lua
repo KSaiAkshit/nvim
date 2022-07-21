0f0da0dd6b75c2c2497db3dc48baf51647e9e34c
Sai Akshit <44054643+KSaiAkshit@users.noreply.github.com>
2022-07-21 16:08:02 +0400

    Added Colorscheme

---
+++ lua/user/colorscheme.lua
@@ -0,0 +1,9 @@
+local colorscheme = "onedarker"
+
+vim.g.transparent_background = true
+
+local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
+if not status_ok then
+  vim.notify("colorscheme " .. colorscheme .. " not found!")
+  return
+end
